0c36de5ba522d4a63650ce083eb2343c51b98629
Idan Elhassid <112956707+Idan-Elhassid@users.noreply.github.com>
2022-12-14 04:11:23 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const gallery = document.getElementById("gallery");
+
+window.onmousemove = e => {
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+
+    const xDecimal = mouseX /window.innerWidth;
+    const yDecimal = mouseY /window.innerHeight;
+
+    const maxX = gallery.offsetWidth - window.innerWidth;
+    const maxY = gallery.offsetWidth - window.innerHeight;
+
+    const panX = maxX * xDecimal * -1;
+    const panY = maxY * yDecimal * -1;
+
+    gallery.animate({
+        transform: `translate(${panX}px, ${panY}px)`
+    }, {
+        duration: 5000,
+        fill: "forwards",
+        easing: "ease"
+    })
+};
+//var x = document.getElementById("1");
+
+//function playA(){
+//    x.play();
+//}
+
+const pics = document.querySelectorAll(".pics");
+
+pics.forEach(function(pic){
+    pic.addEventListener("click", function(event){
+    const newPic = event.currentTarget;
+    const currentDiv = newPic.parentElement;
+    const audioFile = currentDiv.querySelector(".audio");
+    audioFile.play()
+    });
+});
